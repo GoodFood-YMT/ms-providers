5fd2511da39b50f08d0c9c081b6bdcd7ed5ae6ff
MatthieuGallice <frooztor@gmail.com>
2023-04-20 12:41:58 +0200

    Reglage Update: Post => patch

--- app/Controllers/Http/ProvidersController.ts
+++ app/Controllers/Http/ProvidersController.ts
@@ -1,14 +1,22 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Provider from 'App/Models/Provider'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
 export default class ProvidersController {
-  public async index({ response }: HttpContextContract) {
-    const providers = await Provider.all()
+  public async index({ request, response }: HttpContextContract) {
+    const page = request.input('page', 1)
+    const limit = 10
+    const providers = await Provider.query().paginate(page, limit)
     return response.status(200).json(providers)
   }
 
   public async store({ request, response }: HttpContextContract) {
-    const data = request.only(['name'])
+    const validationSchema = schema.create({
+      name: schema.string(),
+    })
+    const data = await request.validate({
+      schema: validationSchema,
+    })
     const provider = await Provider.create(data)
     return response.status(200).json(provider)
   }
@@ -19,7 +27,12 @@ export default class ProvidersController {
   }
 
   public async update({ params, request, response }: HttpContextContract) {
-    const data = request.only(['name'])
+    const validationSchema = schema.create({
+      name: schema.string(),
+    })
+    const data = await request.validate({
+      schema: validationSchema,
+    })
     const provider = await Provider.findOrFail(params.id)
     provider.name = data.name
     await provider.save()
